44631fc0832559d13b0ab8574d7fbd2dbb301a64
Yszty <m.michalski@o2.pl>
2023-02-28 14:09:57 +0100

    cleaning

--- 10-set-env.sh
+++ 10-set-env.sh
@@ -26,10 +26,3 @@ sed -i "s/DB_PASSWORD.*=.*//g" $ENV_PATH &&
 echo "DB_PASSWORD=\"$(cat "$MARIADB_USER_PASSWORD_FILE" | head -n 1)\"" >> $ENV_PATH;
 
 
-
-
-#sed -i "s/define\s*(\s*'DB_USER'\s*,.*);/define( 'DB_USER', '$APP_DB_USER' );/g" /app/wp-config.php
-#sed -i "s/define\s*(\s*'DB_PASSWORD'\s*,.*);/define( 'DB_PASSWORD', '$APP_DB_PASSWORD' );/g" /app/wp-config.php
-#sed -i "s/define\s*(\s*'DB_HOST'\s*,.*);/define( 'DB_HOST', '$APP_DB_HOST' );/g" /app/wp-config.php
-#
-#echo 'Done'
