2bb13861a110682fa6d88db7817bc488106b25ce
Yszty <m.michalski@o2.pl>
2023-02-28 12:51:55 +0100

    core

---
+++ 01-db-init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+
+export MARIADB_USER_PASSWORD=$(cat $MARIADB_USER_PASSWORD_FILE)
+MARIADB_ROOT_PASSWORD=$(cat $MARIADB_ROOT_PASSWORD_FILE)
+
+#envsubst < "/sql-templates/create-users.sql.template"
+#envsubst < "/sql-templates/create-database.sql.template"
+
+mariadb -h db -uroot -p"$MARIADB_ROOT_PASSWORD" < <(envsubst < "/sql-templates/create-users.sql.template")
+
+
+
+
