6a3eb35d37c83e392ff7ef59e918ca13000ec851
Yszty <m.michalski@o2.pl>
2023-02-28 13:16:08 +0100

    fix

--- 01-db-init.sh
+++ 01-db-init.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+absolute_script_dir_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$absolute_script_dir_path"
 
 
 export MARIADB_USER_PASSWORD=$(cat $MARIADB_USER_PASSWORD_FILE)
 MARIADB_ROOT_PASSWORD=$(cat $MARIADB_ROOT_PASSWORD_FILE)
 
+
 #envsubst < "/sql-templates/create-users.sql.template"
 #envsubst < "/sql-templates/create-database.sql.template"
+
 
 mariadb -h db -uroot -p"$MARIADB_ROOT_PASSWORD" < <(envsubst < "./sql-templates/create-users.sql.template")
 
